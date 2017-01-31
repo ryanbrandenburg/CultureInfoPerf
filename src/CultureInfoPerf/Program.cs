907d88119a9fd60c2eddf71ffa5ae217f70d9abd
Ryan Brandenburg <rybrande@microsoft.com>
2017-01-31 15:09:22 -0800

    Initial commit

---
+++ src/CultureInfoPerf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace CultureInfoPerf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cultureCache = new List<CultureInfo>();
+
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (var culture in cultures)
+            {
+                cultureCache.Add(new CultureInfo(culture.Name));
+            }
+            stopwatch.Stop();
+
+            var average = stopwatch.ElapsedTicks / cultures.Length;
+            Console.WriteLine($"There were {cultures.Length} cultures");
+            Console.WriteLine($"Took {stopwatch.ElapsedTicks} ticks");
+            Console.WriteLine($"Took {average} ticks per culture");
+        }
+    }
+}
